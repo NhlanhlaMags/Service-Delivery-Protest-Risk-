0a3df872cf6bdba2e29fc87c08a83a126af43231
NhlanhlaMags <69971222+NhlanhlaMags@users.noreply.github.com>
2025-08-19 07:55:42 +0200

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -255,8 +255,7 @@ with tab2:
                     ) 
             except Exception as e:
                 st.error(f"Error processing file: {str(e)}")
-except:
-    print(error("Model could not be loaded. Please check the file path and try again.")) 
+except st(error("Model could not be loaded. Please check the file path and try again.")) 
 
                
 
